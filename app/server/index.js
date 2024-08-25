2f6f874946d75f4242b681c7773cb46053967f28
bikramdhami0100 <bikramdhami0100@gmail.com>
2024-08-25 23:57:34 +0545

    yesh

---
+++ app/server/index.js
@@ -0,0 +1,34 @@
+const  {Server}=require("socket.io");
+const io=new Server(8000,{
+    cors:true
+});
+const emailToSocketIdMap=new Map();
+const socketidToEmailMap=new Map();
+io.on("connection",(socket)=>{
+    // console.log("socket connected",socket.id);
+ 
+    socket.on("room:join",(data)=>{
+        console.log(data);
+        const {email,roomId}=data;
+        emailToSocketIdMap.set(email,socket.id);
+        socketidToEmailMap.set(socket.id,email);
+        io.to(roomId).emit("user:joined",{email,id:socket.id});
+        socket.join(roomId);
+        io.to(socket.id).emit("room:join",data);
+    });
+    socket.on("user:call",({to,offer})=>{
+        io.to(to).emit("incomming:call",{from:socket.id,offer})
+    })
+    socket.on("call:accepted",({to,answer})=>{
+        io.to(to).emit("call:accepted",{from:socket.id,answer})
+    })
+    // peer:nego:needed;
+    socket.on("peer:nego:needed",({to,offer})=>{
+        io.to(to).emit("peer:nego:needed",{from:socket.id,offer})
+    })
+    
+    // done
+    socket.on("peer:nego:done",({to,answer})=>{
+        io.to(to).emit("peer:nego:final",{from:socket.id,answer})
+    })
+})
